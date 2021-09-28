b5bcc159fe49b9b787c251290b86537f287d83df
Bradley Nixon <bnixon@xmhawaii.com>
2021-09-27 20:09:29 -0700

    modified hello.js and added Procfile

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000;
 
 http.createServer(function(req, res) {
   var q = url.parse(req.url, true);
@@ -16,6 +17,4 @@ http.createServer(function(req, res) {
     res.write(data);
     return res.end();
   })  
-}).listen(8080);
-
-console.log("Server listening on Port 8080...");
+}).listen(PORT);
--- readme.md
+++ readme.md
@@ -2,3 +2,16 @@ First: git init
 Type: git add .
 Then: git commit -am "commit message"
 Finally: git push
+
+To push to Heroku
+First: npm init (creates package.json)
+Next: Edit package.json {
+  Just above "test": type...
+  "start": "node hello.js",
+}
+Then: add a new variable for the port {
+  const PORT = process.env.PORT || 5000;
+  modify .listen(PORT);
+}
+Then: Heroku login
+Next: Heroku create
